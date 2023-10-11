9a9bf5c8a45c6f8fd6bc5fab13fd7aa978aeeb79
Teodor <teodorpascu2001@gmail.com>
2023-10-11 15:52:16 +0200

    Creazione dell'app Flutter di default con telefono funzionante

---
+++ progetto_mobile_computing_ila_e_teo/android/app/src/main/kotlin/com/example/progetto_mobile_computing_ila_e_teo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.progetto_mobile_computing_ila_e_teo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ progetto_mobile_computing_ila_e_teo/flutter_progetto_ila_e_teo/android/app/src/main/kotlin/com/example/flutter_progetto_ila_e_teo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_progetto_ila_e_teo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
