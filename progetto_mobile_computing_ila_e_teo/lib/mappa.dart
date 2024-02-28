ced34b5ed6ce4f36d9184d0f5e2da6d3868334db
Teodor <teodorpascu2001@gmail.com>
2024-02-28 13:13:12 +0100

    Mappa aggiornata con il percorso

--- progetto_mobile_computing_ila_e_teo/lib/mappa.dart
+++ progetto_mobile_computing_ila_e_teo/lib/mappa.dart
@@ -1,39 +1,75 @@
-// ignore_for_file: prefer_const_constructors
-
+// ignore_for_file: prefer_const_constructors, non_constant_identifier_names
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class Mappa extends StatefulWidget {
   const Mappa({super.key});
+
   @override
   State<Mappa> createState() => MappaPageState();
 }
 
-class MappaPageState extends State<Mappa>{
+class MappaPageState extends State<Mappa> {
+  // Coordinate GPS di Roma Termini e Colosseo
+  final LatLng RomaTermini = LatLng(41.9028, 12.4964); // Roma Termini
+  final LatLng Colosseo = LatLng(41.8902, 12.4922); // Colosseo
+
   @override
-Widget build(BuildContext context) {
-  return FlutterMap(
-    options: MapOptions(
-      initialCenter: LatLng(41.902782, 12.496366),
-      initialZoom: 9.2,
-    ),
-    children: [
-      TileLayer(
-        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-        userAgentPackageName: 'com.example.app',
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Progetto Ila e Teo'),
       ),
-      RichAttributionWidget(
-        attributions: [
-          TextSourceAttribution(
-            'PROVA',
-            onTap: () =>
-                launchUrl(Uri.parse('https://openstreetmap.org/copyright')),
+      body: FlutterMap(
+        options: MapOptions(
+          initialCenter: RomaTermini, // Centro della mappa
+          initialZoom: 14.0, // Livello di zoom iniziale
+        ),
+        children: [
+          // Aggiungi un layer di mappa OpenStreetMap
+          TileLayer(
+            urlTemplate: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+            subdomains: const ['a', 'b', 'c'],
+          ),
+          // Aggiungi un layer per il percorso
+          PolylineLayer(
+            polylines: [
+              Polyline(
+                points: [RomaTermini, Colosseo], // Punti del percorso
+                color: Colors.blue, // Colore del percorso
+                strokeWidth: 3.0, // Spessore del percorso
+              ),
+            ],
+          ),
+          // Aggiungi un marker per Roma Termini
+          MarkerLayer(
+            markers: [
+              Marker(
+                width: 40.0,
+                height: 40.0,
+                point: RomaTermini,
+                child: Icon(
+                  Icons.location_on,
+                  color: Colors.green,
+                  size: 40.0,
+                ),
+              ),
+              // Aggiungi un marker per il Colosseo
+              Marker(
+                width: 40.0,
+                height: 40.0,
+                point: Colosseo,
+                child: Icon(
+                  Icons.location_on,
+                  color: Colors.red,
+                  size: 40.0,
+                ),
+              ),
+            ],
           ),
         ],
       ),
-    ],
-  );
-}
+    );
+  }
 }
